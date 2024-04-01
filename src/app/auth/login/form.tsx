c9a6ea1163bc256abc7be8ef32382908e939ec28
Igor PC <igor_roc@hotmail.com.br>
2024-04-01 18:50:08 -0300

    feat: Loading state

--- src/app/auth/login/form.tsx
+++ src/app/auth/login/form.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react"
 import { Role } from "@prisma/client"
 import { toast } from "react-toastify"
 import { useRouter } from "next/navigation"
@@ -9,12 +10,15 @@ import { loginAction } from "@/actions/auth/login"
 
 export default function Form() {
 	const router = useRouter()
+	const [loading, setLoading] = useState(false)
 
 	async function handleSubmit(formData: FormData) {
+		setLoading(true)
 		const res = await loginAction(formData)
 
 		if ("error" in res) {
 			toast.error(res.error)
+			setLoading(false)
 			return
 		}
 
@@ -24,14 +28,16 @@ export default function Form() {
 		} else {
 			toast.error("Você não tem permissão para acessar esta página")
 		}
+
+		setLoading(false)
 	}
 
 	return (
 		<form action={handleSubmit} className="flex flex-col items-center gap-2">
-			<Input name="email" type="email" label="E-mail" />
-			<Input name="password" type="password" label="Senha" />
+			<Input isDisabled={loading} name="email" type="email" label="E-mail" />
+			<Input isDisabled={loading} name="password" type="password" label="Senha" />
 
-			<Button type="submit" color="primary" className="w-full">
+			<Button isLoading={loading} type="submit" color="primary" className="w-full">
 				Entrar
 			</Button>
 		</form>
