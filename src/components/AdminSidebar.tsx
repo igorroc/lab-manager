075f1d443dbfca7c2258db751985d5bbf3ce2495
Igor PC <igor_roc@hotmail.com.br>
2024-04-01 19:34:29 -0300

    feat: Admin Sidebar working

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,53 +1,86 @@
+"use client"
+
 import Link from "next/link"
-import React from "react"
+import { usePathname } from "next/navigation"
+
+import { FaBook, FaChalkboardTeacher, FaClock, FaCog, FaHome } from "react-icons/fa"
+import { FaPeopleGroup } from "react-icons/fa6"
+import { MdMeetingRoom } from "react-icons/md"
+
 import LogoutButton from "./LogoutButton"
 
 export default function AdminSidebar() {
+	const pathname = usePathname()
+
 	const routes = [
 		{
 			name: "Dashboard",
-			icon: "",
+			icon: <FaHome />,
 			href: "/super/dashboard",
 		},
 		{
-			name: "Usuários",
-			icon: "",
-			href: "/super/users",
+			name: "Salas de Aula",
+			icon: <MdMeetingRoom />,
+			href: "/super/dashboard/classrooms",
 		},
 		{
-			name: "Laboratórios",
-			icon: "",
-			href: "/super/labs",
+			name: "Disciplinas",
+			icon: <FaBook />,
+			href: "/super/dashboard/subjects",
 		},
 		{
-			name: "Equipamentos",
-			icon: "",
-			href: "/super/equipments",
+			name: "Professores",
+			icon: <FaChalkboardTeacher />,
+			href: "/super/dashboard/professors",
 		},
 		{
-			name: "Agendamentos",
-			icon: "",
-			href: "/super/schedules",
+			name: "Turmas",
+			icon: <FaPeopleGroup />,
+			href: "/super/dashboard/class-groups",
+		},
+		{
+			name: "Horários",
+			icon: <FaClock />,
+			href: "/super/dashboard/schedules",
+		},
+		{
+			name: "Configurações",
+			icon: <FaCog />,
+			href: "/super/dashboard/settings",
 		},
 	]
+
+	function checkCurrentRoute(routePath: string) {
+		return pathname === routePath
+	}
+
 	return (
 		<aside className="fixed w-64 bg-slate-800 h-dvh">
-			<div>
-				<h1>Lab Manager</h1>
-			</div>
-			<div className="flex flex-col gap-2 p-4">
-				{routes.map((route) => (
-					<Link
-						key={route.href}
-						href={route.href}
-						className="p-2 hover:bg-slate-700 rounded-md transition-all"
-					>
-						{route.name}
-					</Link>
-				))}
-			</div>
-			<div>
-				<LogoutButton />
+			<div className="h-full flex flex-col justify-between">
+				<div className="flex flex-col gap-2">
+					<div className="flex items-center justify-center py-4">
+						<h1>Lab Manager</h1>
+					</div>
+					<div className="flex flex-col gap-2 p-4">
+						{routes.map((route) => (
+							<Link
+								key={route.href}
+								href={route.href}
+								className={`p-2 hover:bg-slate-700 rounded-md transition-all flex gap-2 items-center ${
+									checkCurrentRoute(route.href)
+										? "bg-slate-700 text-white opacity-100"
+										: "opacity-40"
+								}`}
+							>
+								{route.icon && <span>{route.icon}</span>}
+								{route.name}
+							</Link>
+						))}
+					</div>
+				</div>
+				<div className="p-4 flex flex-col gap-2 items-center justify-center">
+					<LogoutButton />
+				</div>
 			</div>
 		</aside>
 	)
