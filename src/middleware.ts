e6c914057ad0f592dc60961f61bab0337877df86
Igor PC <igor_roc@hotmail.com.br>
2024-04-03 00:08:59 -0300

    fix: Middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,24 @@ import { NextRequest } from "next/server"
 import { getUserBySession } from "@/modules/auth"
 
 export async function middleware(request: NextRequest) {
-	const user = await getUserBySession()
-
-	if (user) {
-		if (
-			request.nextUrl.pathname.startsWith("/auth") &&
-			!request.nextUrl.pathname.startsWith("/auth/logout")
-		) {
+	if (
+		request.nextUrl.pathname.startsWith("/auth") &&
+		!request.nextUrl.pathname.startsWith("/auth/logout")
+	) {
+		const user = await getUserBySession()
+		if (user) {
 			if (user.role == Role.USER) {
 				return Response.redirect(new URL("/dashboard", request.url))
 			} else if (user.role == Role.ADMIN) {
 				return Response.redirect(new URL("/super/dashboard", request.url))
 			}
 		}
-	} else {
-		if (!request.nextUrl.pathname.startsWith("/auth")) {
+	}
+
+	if (request.nextUrl.pathname.startsWith("/super")) {
+		const user = await getUserBySession()
+
+		if (!user || user.role != Role.ADMIN) {
 			return Response.redirect(new URL("/auth/login", request.url))
 		}
 	}
