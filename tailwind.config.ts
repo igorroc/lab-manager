c0c15b67319c21778dee81f5672098463a64c41a
Igor PC <igor_roc@hotmail.com.br>
2024-06-15 15:54:47 -0300

    fix: Change primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config: Config = {
 			boxShadow: {
 				"up-md": "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
 			},
+			colors: {
+				primary: "#0BA359",
+			},
 		},
 	},
 	plugins: [
